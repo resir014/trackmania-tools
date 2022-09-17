aadd5264d76399e5c157bc8b314b39df0882bf78
Resi Respati <resir014@gmail.com>
2022-09-17 08:31:24 +0700

    feat: updated footer

--- src/modules/home/home-footer.tsx
+++ src/modules/home/home-footer.tsx
@@ -3,7 +3,18 @@ import * as React from 'react';
 export function HomeFooter() {
   return (
     <footer className="px-6 pb-8 pt-4 text-center">
-      <p className="text-gray-400 text-sm">&copy; 2020 resir014. MIT licenced.</p>
+      <p className="text-gray-400 text-sm">
+        &copy; 2022 resir014.{' '}
+        <a
+          className="text-green-500 hover:underline"
+          href="https://github.com/resir014/trackmania-tools"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>{' '}
+        available under the MIT licence.
+      </p>
       <p className="text-gray-400 text-sm">
         This project is not affiliated with Trackmania or Ubisoft Nadeo.
       </p>
