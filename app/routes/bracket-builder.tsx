2902e3e40d9610a937d9ca737feb4f33e5aba4d5
Resi Respati <resir014@gmail.com>
2025-10-02 17:51:33 +0700

    chore: add meta tags

--- app/routes/bracket-builder.tsx
+++ app/routes/bracket-builder.tsx
@@ -4,13 +4,25 @@ import { Page } from '~/components/page';
 import { LoadingPlaceholder } from '~/components/ui/loading';
 import siteMetadata from '~/data/site-metadata';
 
+const metaTitle = 'Bracket Builder';
+const metaDescription = 'A supplementary tool to generate brackets for the official Trackmania competition tool.';
+
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: `Bracket Builder · ${siteMetadata.title}` },
-    {
-      name: 'description',
-      content: 'A supplementary tool to generate brackets for the official Trackmania competition tool.',
-    },
+    { title: `${metaTitle} · ${siteMetadata.title}` },
+    { name: 'description', content: metaDescription },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: siteMetadata.title },
+    { name: 'twitter:creator', content: '@resir014' },
+    { property: 'og:description', content: metaDescription },
+    { property: 'og:image', content: `${siteMetadata.siteUrl}/social.png` },
+    { property: 'og:image:alt', content: siteMetadata.title },
+    { property: 'og:image:width', content: 1200 },
+    { property: 'og:image:height', content: 630 },
+    { property: 'og:site_name', content: siteMetadata.title },
+    { property: 'og:title', content: metaTitle },
+    { property: 'og:url', content: `${siteMetadata.siteUrl}${location.pathname}` },
+    { property: 'og:type', content: 'website' },
   ] satisfies Route.MetaDescriptors;
 }
 
